34b3579012d619a27ab77ac7657ba8b571f6fa7d
Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>
2020-03-31 22:24:34 -0300

    import sqlalchemty and configure

--- models/hotel.py
+++ models/hotel.py
@@ -1,4 +1,14 @@
-class HotelModel:
+from sql_alchemy import banco
+
+class HotelModel(banco.Model):
+    __tablename__ = 'hoteis' 
+
+    hotel_id = banco.Column(banco.String, primary_key=True)
+    nome = banco.Column(banco.String)
+    estrelas = banco.Column(banco.Float(precision = 1))
+    diaria = banco.Column(banco.Float(precision = 2))
+    cidade = banco.Column(banco.String)
+
     def __init__(self, hotel_id, nome, estrelas, diaria, cidade):
         self.hotel_id = hotel_id
         self.nome = nome
@@ -14,3 +24,21 @@ class HotelModel:
             'daria': self.diaria,
             'cidade': self.cidade
         }
+
+    @classmethod
+    def find_hotel(cls, hotel_id):
+        hotel = cls.query.filter_by(hotel_id=hotel_id).first()  # select *from hoteis where hotel_id = x limit 1
+        if hotel:
+            return hotel
+        return None
+
+    def save_hotel(self):
+        banco.session.add(self)
+        banco.session.commit()
+
+    def update_hotel(self, nome, estrelas, diaria, cidade):
+        self.nome = nome
+        self.estrelas = estrelas
+        self.diaria = diaria
+        self.cidade = cidade
+        
